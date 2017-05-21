b27174d7f3b0519d49eb9880f5129290c780196d
david-sn <david.sn@yahoo.com>
2017-05-21 18:32:01 +0200

    InitDB Done

--- src/main/java/com/david/webmonitor/Hibernate/Sessions/InitDB.java
+++ src/main/java/com/david/webmonitor/Hibernate/Sessions/InitDB.java
@@ -4,19 +4,31 @@ import com.david.webmonitor.Hibernate.DAO.CheckDAO;
 import com.david.webmonitor.Hibernate.Entity.Checkers;
 import org.springframework.beans.factory.annotation.Autowired;
 
-
 public class InitDB {
-    
+
     @Autowired
     CheckDAO checkDAO;
-    
-    public void init(){
-            
-        Checkers c =new Checkers();
-        c.setName("asdsda");
-        
-        checkDAO.createCheckers(c);
-        
+
+    public void init() {
+
+        {
+            Checkers c = new Checkers();
+            c.setName("google");
+            c.setUrl("www.google.com");
+            checkDAO.createCheckers(c);
+        }
+        {
+            Checkers c = new Checkers();
+            c.setName("example");
+            c.setUrl("www.example.com");
+            checkDAO.createCheckers(c);
+        }
+        {
+            Checkers c = new Checkers();
+            c.setName("twitter");
+            c.setUrl("www.twitter.com");
+            checkDAO.createCheckers(c);
+        }
     }
-    
+
 }
